a6d2a4fd425a37c6fee8f7f1c3d53a5d931df1d5
NKT <khoami123678@gmail.com>
2023-10-10 17:16:19 +0700

    get Detail Doctor

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -79,8 +79,54 @@ let saveInforDoctor = (inputData) => {
   })
 }
 
+let getDetailDoctorById = (inputId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!inputId) {
+        resolve({
+          errCode: 1,
+          errMessage: 'Missing required parameter!'
+        })
+      } else {
+        let data = await db.User.findOne({
+          where: {
+            id: inputId
+          },
+          attributes: {
+            exclude: ['password']
+          },
+          include: [
+            {
+              model: db.Markdown,
+              attributes: ['contentHTML', 'contentMarkdown', 'description']
+            },
+            { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+          ],
+          raw: false,
+          nest: true
+        })
+
+        if (data && data.image) {
+          data.image = new Buffer.from(data.image, 'base64').toString('binary');
+        }
+
+        if (!data) data = {};
+
+        resolve({
+          errCode: 0,
+          data: data
+        })
+      }
+
+    } catch (e) {
+      reject(e);
+    }
+  })
+}
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
-  saveInforDoctor: saveInforDoctor
+  saveInforDoctor: saveInforDoctor,
+  getDetailDoctorById: getDetailDoctorById
 }
